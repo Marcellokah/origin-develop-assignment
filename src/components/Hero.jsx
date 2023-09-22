a374e2e8eb01593b787e8e2265779668a27fcbfa
Marcellokah <aalma47@gmail.com>
2023-09-22 10:25:40 +0200

    Added animations to the header section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { styles } from '../styles';
+import 'animate.css';
 
 const Hero = () => {
   return (
@@ -7,13 +8,13 @@ const Hero = () => {
       <div className="flex flex-col justify-start gap-8">
 
         {/* Time, title, paragraph */}
-        <p className="font-light text-primary text-xs">29.Jun.2021</p>
-        <p className="font-bold text-secondary text-2xl">Aron Klein’s captivating images of the UDEA’s culture.</p>
-        <p className="font-light text-primary text-xs">Many years ago, I worked for my parents who own a video production company.</p>
+        <p className="font-light text-primary text-xs animate__animated animate__fadeInLeft">29.Jun.2021</p>
+        <p className="font-bold text-secondary text-2xl animate__animated animate__fadeInLeftBig">Aron Klein’s captivating images of the UDEA’s culture.</p>
+        <p className="font-light text-primary text-xs animate__animated animate__fadeInLeft">Many years ago, I worked for my parents who own a video production company.</p>
 
         {/* Details */}
-        <div className='border-t-2 pt-5 w-full'>
-          <ul ul className='list-none flex flex-row gap-12 font-light text-primary text-xs '>
+        <div className='border-t-2 pt-5 w-full animate__animated animate__fadeIn animate__delay-1s'>
+          <ul ul className='list-none flex flex-row gap-12 font-light text-primary text-xs animate__animated animate__fadeInLeft animate__delay-1s'>
             <li>Peter Rowardson</li>
             <li>Design Trend</li>
             <li>2 min</li>
