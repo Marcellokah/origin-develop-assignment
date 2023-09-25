5e92f75a925448b81b2835ecd483ef385279de4d
Marcellokah <aalma47@gmail.com>
2023-09-25 11:55:43 +0200

    Search icon mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto leading-6'>
 
         {/* Vercel logo */}
-        <h3 className='uppercase font-bold text-lg text-secondary'>vercel<span className='text-tertiary'>.</span></h3>
+        <h3 className='uppercase font-bold text-lg text-secondary cursor-default'>vercel<span className='text-tertiary'>.</span></h3>
 
         {/* Navigation links */}
         <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -45,12 +45,12 @@ const Navbar = () => {
         <div className='flex justify-end gap-5'>
           <ul className='list-none hidden sm:flex flex-row gap-10 border-r-2 py-1 pr-5'>
             {socialLinks.map((link) => (
-              <li key={link.id} className={`${active === link.title ? "text-primary underline underline-offset-8 decoration-tertiary" : "text-primary/50"} hover:text-primary hover:underline hover:decoration-tertiary hover:underline-offset-8 text-xs font-medium cursor-pointer`} onClick={() => setActive(link.title)}>
+              <li key={link.id} className='text-primary/50 hover:text-primary text-xs font-medium cursor-pointer'>
                 <a href={link.url}>{link.title}</a>
               </li>
             ))}
           </ul>
-          <BiSearch className='text-primary/50 hover:text-primary hover:underline hover:decoration-tertiary hover:underline-offset-8 text-sm cursor-pointer mt-1.5'/>
+          <BiSearch className='hidden sm:flex text-primary/50 hover:text-primary hover:underline hover:decoration-tertiary hover:underline-offset-8 text-sm cursor-pointer mt-1.5 ' />
         </div>
 
         {/* Mobile navbar */}
@@ -59,24 +59,24 @@ const Navbar = () => {
           <div className={`${!toggle ? 'hidden' : 'flex justify-center gap-20 items-center'} p-6 absolute top-20 mx-4 my-2 bg-tertiary z-10 rounded-xl`}>
             <ul className='list-none flex justify-end items-start flex-col gap-4'>
               {navLinks.map((link) => (
-                <li key={link.id} className={`${active === link.title ? "text-primary underline underline-offset-8 decoration-tertiary" : "text-primary/50"} font-poppins font-medium cursor-pointer text-xs`} onClick={() => {
+                <li key={link.id} className={`${active === link.title ? "text-primary underline underline-offset-8 decoration-primary" : "text-primary/50"} font-poppins font-medium cursor-pointer text-xs`} onClick={() => {
                   setToggle(!toggle);
                   setActive(link.title);
                 }}>
-                  <a href={`/${link.id}`}>{link.title}</a>
+                  <a href={`#${link.id}`}>{link.title}</a>
                 </li>
               ))}
             </ul>
-            <ul className='list-none flex justify-end items-start flex-col gap-4'>
-              {socialLinks.map((link) => (
-                <li key={link.id} className={`${active === link.title ? "text-primary underline underline-offset-8 decoration-tertiary" : "text-primary/50"} font-poppins font-medium cursor-pointer text-xs`} onClick={() => {
-                  setToggle(!toggle);
-                  setActive(link.title);
-                }}>
-                  <a href={link.url}>{link.title}</a>
-                </li>
-              ))}
-            </ul>
+            <div className='flex flex-col gap-4'>
+              <ul className='list-none flex justify-end items-start flex-col gap-4'>
+                {socialLinks.map((link) => (
+                  <li key={link.id} className='text-primary/50 hover:text-primary font-medium cursor-pointer text-xs'>
+                    <a href={link.url}>{link.title}</a>
+                  </li>
+                ))}
+              </ul>
+              <BiSearch className='text-primary/50 text-sm cursor-pointer' />
+            </div>
           </div>
         </div>
       </div>
